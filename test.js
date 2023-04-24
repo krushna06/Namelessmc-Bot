9414dbe01cd7b35c5c11aac3549c317b6b16d5cd
Dornox <krushnavyas06@gmail.com>
2023-04-24 15:20:47 +0000

    Groupinfo

--- test.js
+++
@@ -1,9 +0,0 @@
-const { COOLDOWN } = require('../../JSON/config.json');
-
-module.exports = {
-    name: 'test',
-    aliases: ['t'],
-    cooldown: COOLDOWN,
-    execute(client, message, args) {
-    }
-}
